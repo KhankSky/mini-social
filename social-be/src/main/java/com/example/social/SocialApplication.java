96e7d3fd575824f449ac8a961d8490501e3d5be4
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-11-08 10:21:17 +0700

    disable security

--- social-be/src/main/java/com/example/social/SocialApplication.java
+++ social-be/src/main/java/com/example/social/SocialApplication.java
@@ -1,9 +1,13 @@
 package com.example.social;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
+)
+//@SpringBootApplication
 public class SocialApplication {
 
 	public static void main(String[] args) {
