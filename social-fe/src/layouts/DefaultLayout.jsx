48440d118dffd0944b06337e4b912dbb22fb4afa
KhankSky <pkhanhsnd037@gmail.com>
2025-11-17 09:39:10 +0700

    feat: add Header component and update DefaultLayout for improved structure

--- social-fe/src/layouts/DefaultLayout.jsx
+++ social-fe/src/layouts/DefaultLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
+import Header from "./Header.jsx";
 
 const DefaultLayout = () => {
   return (
-    <div>
-      <Outlet/>
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <main className="container mx-auto px-4 py-6">
+        <Outlet />
+      </main>
     </div>
   );
 };
