44b9c2b55ba9008d3fa5d502679cc8dc5240b564
KhankSky <pkhanhsnd037@gmail.com>
2025-12-30 22:07:18 +0700

    fix date

--- social-be/src/main/java/com/example/social/SocialApplication.java
+++ social-be/src/main/java/com/example/social/SocialApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 // disable security
 //@SpringBootApplication(exclude = {
 //        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
 //)
 @SpringBootApplication
 public class SocialApplication {
+
+	@PostConstruct
+	public void init() {
+		// Set timezone to Vietnam (UTC+7)
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
